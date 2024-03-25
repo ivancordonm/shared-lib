458222c5bae0653a13cc7a193f50e9dac6636eae
Ivan <ivancordonm@gmail.com>
2024-03-25 17:22:17 +0100

    write pom

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -34,27 +34,30 @@ def call(Map pipeline_config = [:]) {
                         def version
                         if (pom_version.contains('SNAPSHOT')) {
                             version = pom_version.replace('-SNAPSHOT', '')
-                            logger.info("version: ${version.split('\\.')}")
-                            switch (config.version.upgrade) {
-                                case 'major':
-                                    def s = version.split('\\.')
-                                    version = "${s[0].toInteger() + 1}.${s[1]}.${s[2]}"
-                                    break
-                                case 'minor':
-                                    def v = version.split('\\.')
-                                    version = "${v[0]}.${v[1].toInteger() + 1}.${v[2]}"
-                                    break
-                                case 'patch':
-                                    def v = version.split('\\.')
-                                    version = "${v[0]}.${v[1]}.${v[2].toInteger() + 1}"
-                                    break
-                                default:
-                                    break
-                            }
-
-                            logger.info("new version: ${version}")
-
+                        } else {
+                            version = pom_version
                         }
+                        logger.info("version: ${version.split('\\.')}")
+                        switch (config.version.upgrade) {
+                            case 'major':
+                                def s = version.split('\\.')
+                                version = "${s[0].toInteger() + 1}.${s[1]}.${s[2]}"
+                                break
+                            case 'minor':
+                                def v = version.split('\\.')
+                                version = "${v[0]}.${v[1].toInteger() + 1}.${v[2]}"
+                                break
+                            case 'patch':
+                                def v = version.split('\\.')
+                                version = "${v[0]}.${v[1]}.${v[2].toInteger() + 1}"
+                                break
+                            default:
+                                break
+                        }
+
+                        logger.info("new version: ${version}")
+                        pom.version = version
+                        writeMavenPom file: 'pom.xml', model: pom
                     }
                 }
             }
