d98b2113b8fb0f6997a54dc57c69c500500dcc1c
Ivan <ivancordonm@gmail.com>
2024-03-25 16:54:23 +0100

    upgrade version

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -29,6 +29,32 @@ def call(Map pipeline_config = [:]) {
                         } catch (FileNotFoundException e) {
                             logger.error("Error: ${e}")
                         }
+                        def pom_version = pom.version
+                        logger.info("version: ${pom_version}")
+                        def version
+                        if (pom_version.contains('SNAPSHOT')) {
+                            version = pom_version.replace('-SNAPSHOT', '')
+                            switch (config.version.upgrade) {
+                                case 'major':
+                                    version = version.split('\\.')[0].toInteger() + 1
+                                    version = "${version}.0.0"
+                                    break
+                                case 'minor':
+                                    version = version.split('\\.')[1].toInteger() + 1
+                                    version = "${version}.0"
+                                    break
+                                case 'patch':
+                                    version = version.split('\\.')[2].toInteger() + 1
+                                    version = "${version}"
+                                    break
+                                default:
+                                    version = version
+                                    break
+                            }
+
+                            logger.info("new version: ${version}")
+
+                        }
                     }
                 }
             }
