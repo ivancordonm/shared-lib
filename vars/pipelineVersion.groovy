4178420ef61c316e3a9ffc1665958edebacfe20f
Ivan <ivancordonm@gmail.com>
2024-03-25 15:10:57 +0100

    add step

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -3,5 +3,16 @@ import utils.Logger
 def call(Map pipeline_config = [:]) {
     def logger = new Logger(this, 'DEBUG')
     logger.debug("pipeline_config: ${pipeline_config}")
-    sh "echo ${pipeline_config['pipelineVersion']} > pipelineVersion"
+    pipeline {
+        agent { label 'linux' }
+        stages {
+            stage('Get Pipeline Version') {
+                steps {
+                    script {
+                        sh "echo ${pipeline_config['pipelineVersion']} > pipelineVersion"
+                    }
+                }
+            }
+        }
+    }
 }
