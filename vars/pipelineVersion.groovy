0f701338beaec1891516a9ea82540efb2c842713
Ivan <ivancordonm@gmail.com>
2024-03-25 15:08:02 +0100

    test if it calls this pipeline

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -3,4 +3,5 @@ import utils.Logger
 def call(Map pipeline_config = [:]) {
     def logger = new Logger(this, 'DEBUG')
     logger.debug("pipeline_config: ${pipeline_config}")
+    sh "echo ${pipeline_config['pipelineVersion']} > pipelineVersion"
 }
