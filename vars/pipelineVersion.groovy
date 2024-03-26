f269d12e8e185f958a2cb378464253aa0692466f
Ivan <ivancordonm@gmail.com>
2024-03-26 11:50:18 +0100

    cheackout

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -66,7 +66,7 @@ def call(Map pipeline_config = [:]) {
                         // commit new version
                         sh "git config --global user.email 'jenkins@localhost'"
                         sh "git config --global user.name 'Jenkins'"
-                        sh "git checkout main"
+                        checkout scm
                         sh "git add pom.xml"
                         sh "git commit -m 'Upgrade version to ${version}'"
                         sh "git push"
