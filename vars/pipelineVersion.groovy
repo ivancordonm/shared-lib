e598dc5508ea6430f9fee8de8c6c6ec55caf4a48
Ivan <ivancordonm@gmail.com>
2024-03-26 13:17:09 +0100

    credentials 2

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -65,11 +65,13 @@ def call(Map pipeline_config = [:]) {
                         // commit new version
                         sh "git config --global user.email 'ivancordonm@gmail.com'"
                         sh "git config --global user.name 'icordonm'"
-                        checkout scm
-                        writeMavenPom file: 'pom.xml', model: pom
-                        sh "git add pom.xml"
-                        sh "git commit -m 'Upgrade version to ${version}'"
-                        sh "git push origin main"
+                        withCredentials([usernamePassword(credentialsId: 'Ivan-Github')]) {
+                            checkout scm
+                            writeMavenPom file: 'pom.xml', model: pom
+                            sh "git add pom.xml"
+                            sh "git commit -m 'Upgrade version to ${version}'"
+                            sh "git push origin main"
+                        }
                         logger.info("Version updated to ${version}")
                     }
                 }
