381c05a19ab59ff58928aec939ff25758cb60c2a
Ivan <ivancordonm@gmail.com>
2024-03-26 13:21:15 +0100

    credentials 4

--- vars/pipelineVersion.groovy
+++ vars/pipelineVersion.groovy
@@ -66,6 +66,7 @@ def call(Map pipeline_config = [:]) {
                         sh "git config --global user.email 'ivancordonm@gmail.com'"
                         sh "git config --global user.name 'icordonm'"
                         withCredentials([usernamePassword(credentialsId: 'Ivan-Github', passwordVariable: 'GITHUB_PASSWORD', usernameVariable: 'GITHUB_USERNAME')]) {
+                            logger.info("credentials: ${env.GITHUB_USERNAME}")
                             checkout scm
                             writeMavenPom file: 'pom.xml', model: pom
                             sh "git add pom.xml"
